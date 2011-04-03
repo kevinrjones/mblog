74397563e4c7dddb11f3585adb4ff14aba92e408
Kevin Jones <kevinj@mantiso.com>
2011-04-03 20:34:24 +0100

    Blogs and Posts joined together

---
+++ MBlogModel/Blog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace MBlogModel
+{
+    public class Blog
+    {
+        public virtual int Id { get; private set; }
+        [Required]
+        public virtual string Title { get; private set; }
+        [Required]
+        public virtual string Description { get; private set; }
+        [Required]
+        public virtual string Nickname { get; private set; }
+
+        public virtual ICollection<Post> Posts { get; set; }
+
+        [ForeignKey("user_id")]
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
