8249c430e5ca9a1861de2e1f897023107b7089df
Kevin Jones <kevinj@mantiso.com>
2011-07-07 15:04:17 +0100

    Updated default.ps1 to also look in x86 directory

--- Deploy/default.ps1
+++ Deploy/default.ps1
@@ -39,5 +39,8 @@ task DeployDatabase -depends Init {
 
 task Deploy -depends Build, DeployDatabase {
     out-host -InputObject $TargetDir
+    if(!(Test-Path $MSDeploy)) {
+        $MSDeploy = C:\Program Files (x86)\IIS\Microsoft Web Deploy V2\msdeploy.exe
+    }
     exec { &$MsDeploy "-verb:sync" "-source:contentPath=$OutputWebDir" "-dest:contentPath=$TargetDir" }
 }
