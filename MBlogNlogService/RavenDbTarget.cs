5b69edc5759d04e4e128edfbe8bb0bc54cad88ea
Kevin Jones <kevinj@mantiso.com>
2011-07-26 13:48:17 +0100

    Initial RavenDB logging code

--- MBlogNlogService/RavenDbTarget.cs
+++ MBlogNlogService/RavenDbTarget.cs
@@ -5,35 +5,20 @@ using NLog;
 using NLog.Common;
 using NLog.Layouts;
 using NLog.Targets;
+using Raven.Client.Document;
 
 namespace MBlogNlogService
 {
     [Target("RavenDb")]
     public class RavenDbTarget : TargetWithLayout
     {
-        public Layout ConnectionString { get; set; }
-        public string ConnectionStringName { get; set; }
-        private ConnectionStringSettingsCollection ConnectionStringsSettings { get; set; }
-
-        [DefaultValue(true)]
-        public bool KeepConnection { get; set; }
-
-        public RavenDbTarget()
-        {
-            ConnectionStringsSettings = ConfigurationManager.ConnectionStrings;    
-        }
+        public string ServerUrl { get; set; }
+        private DocumentStore _store;
 
         protected override void InitializeTarget()
         {
-            bool flag = false;
-            if (!string.IsNullOrEmpty(ConnectionStringName))
-            {
-                ConnectionStringSettings connectionStringSettings = ConnectionStringsSettings[ConnectionStringName];
-                if (connectionStringSettings == null)
-                    throw new NLogConfigurationException("Connection string '" + ConnectionStringName + "' is not declared in <connectionStrings /> section.");
-                ConnectionString = (Layout)SimpleLayout.Escape(connectionStringSettings.ConnectionString);
-                flag = true;
-            }        
+            _store = new DocumentStore {Url = ServerUrl};
+            _store.Initialize();
         }
 
         protected override void Write(LogEventInfo logEvent)
@@ -51,32 +36,18 @@ namespace MBlogNlogService
                 else
                 {
                     InternalLogger.Error("Error when writing to database {0}", new object[]{ex});
-                    CloseConnection();
                     throw;
-                }
-            }
-            finally
-            {
-                if (!KeepConnection)
-                {
-                    CloseConnection();
                 }
             }
         }
 
         private void WriteEventToDatabase(LogEventInfo logEvent)
         {
+            using (var session = _store.OpenSession())
+            {
+                session.Store(logEvent);    
+                session.SaveChanges();
+            }
         }
-
-        protected override void CloseTarget()
-        {
-            base.CloseTarget();
-            CloseConnection();
-        }
-
-        private void CloseConnection()
-        {
-        }
-
     }
 }
