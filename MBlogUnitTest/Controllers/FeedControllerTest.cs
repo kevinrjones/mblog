174e109eb13da5e48e9c56a08791b6b97b65fc90
Kevin Jones <kevinj@mantiso.com>
2011-08-10 19:55:05 +0100

    Tests for FeedController

--- MBlogUnitTest/Controllers/FeedControllerTest.cs
+++ MBlogUnitTest/Controllers/FeedControllerTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Web.Mvc;
 using MBlog.ActionResults;
@@ -32,19 +33,55 @@ namespace MBlogUnitTest.Controllers
             _mockBlogRepository = new Mock<IBlogRepository>();
             MockRequest.Setup(r => r.Url).Returns(new Uri("http://foo.com/feed/rss"));
             var headers = new NameValueCollection();
-            headers.Add("HOST", "foo");
+            headers.Add("HOST", "localhost");
             MockRequest.Setup(r => r.Headers).Returns(headers);
             MockHttpContext.Setup(h => h.Request).Returns(MockRequest.Object);
 
         }
 
         [Test]
-        public void GivenABlogWithNoPosts_WhenIetAFeed_ThenIGetNoItemsInTheFeed()
+        public void GivenABlogWithNoPosts_WhenIGetAFeed_ThenIGetNoItemsInTheFeed()
         {
-            //var controller = new FeedController(_mockBlogRepository.Object, _mockPostRepository.Object, null);
-            //SetControllerContext(controller);
-            //var result = controller.Rss(nickname) as SyndicationActionResult;
-            //Assert.That(result.Feed, Is.Not.Null);
+            var controller = new FeedController(_mockBlogRepository.Object, _mockPostRepository.Object, null);
+            SetControllerContext(controller);
+            _mockBlogRepository.Setup(b => b.GetBlog(It.IsAny<string>())).Returns(new Blog {Description = "description", LastUpdated = new DateTime(), User = new User{Name = "name"}});
+            _mockPostRepository.Setup(p => p.GetBlogPosts(It.IsAny<string>())).Returns(new List<Post>());
+            var result = controller.Rss(nickname) as SyndicationActionResult;
+            Assert.That(result.Feed, Is.Not.Null);
+            Assert.That(result.Feed.Items.Count(), Is.EqualTo(0));
+        }
+
+
+        [Test]
+        public void GivenABlogWithOnePost_WhenIGetAFeed_ThenIGetOneItemInTheFeed()
+        {
+            var controller = new FeedController(_mockBlogRepository.Object, _mockPostRepository.Object, null);
+            SetControllerContext(controller);
+            _mockBlogRepository.Setup(b => b.GetBlog(It.IsAny<string>())).Returns(new Blog { Description = "description", LastUpdated = new DateTime(), User = new User { Name = "name" } });
+            _mockPostRepository.Setup(p => p.GetBlogPosts(It.IsAny<string>())).Returns(new List<Post>{new Post{Title = "title", BlogPost = "post"}});
+            var result = controller.Rss(nickname) as SyndicationActionResult;
+            Assert.That(result.Feed, Is.Not.Null);
+            Assert.That(result.Feed.Items.Count(), Is.EqualTo(1));
+
+        }
+        [Test]
+        public void GivenABlogWithOnePost_WhenIGetAFeed_ThenITheFeedItemHasTheCorrectValues()
+        {
+            var controller = new FeedController(_mockBlogRepository.Object, _mockPostRepository.Object, null);
+            SetControllerContext(controller);
+            _mockBlogRepository.Setup(b => b.GetBlog(It.IsAny<string>())).Returns(new Blog { Description = "description", LastUpdated = new DateTime(), User = new User { Name = "name" } });
+            var post = new Post { Title = "title", BlogPost = "post" };
+            _mockPostRepository.Setup(p => p.GetBlogPosts(It.IsAny<string>())).Returns(new List<Post> { post });
+
+            var url = string.Format("http://localhost/{0}/{1}/{2}/{3}/{4}",  nickname, post.Posted.Year, post.Posted.Month, post.Posted.Day, post.TitleLink);
+
+            var result = controller.Rss(nickname) as SyndicationActionResult;
+            var item = result.Feed.Items.FirstOrDefault();
+
+            Assert.That(item.Title.Text, Is.EqualTo(post.Title));
+            Assert.That(((TextSyndicationContent)item.Content).Text, Is.EqualTo(post.BlogPost));
+            Assert.That(item.PublishDate.DateTime, Is.EqualTo(post.Edited));
+            Assert.That(item.Links[0].Uri, Is.EqualTo(new Uri(url)));
         }
     }
 }
