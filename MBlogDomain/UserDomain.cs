0a26131bc4dd2ad96f0c4af7a9e62e3196a97ad7
Kevin Jones <kevinj@mantiso.com>
2011-12-31 10:38:48 +0000

    Added initial domain project

---
+++ MBlogDomain/UserDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MBlogRepository.Interfaces;
+
+namespace MBlogDomain
+{
+    public class UserDomain
+    {
+        public UserDomain(IUserRepository repository)
+        {
+            
+        }
+    }
+}
