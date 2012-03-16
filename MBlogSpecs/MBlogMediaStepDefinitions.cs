3f3d4de582445848b25f4eb31eb570987ec7b7fb
Kevin Jones <kevinj@mantiso.com>
2012-03-16 16:13:26 +0000

    Acceptance test site can now be published

--- MBlogSpecs/MBlogMediaStepDefinitions.cs
+++ MBlogSpecs/MBlogMediaStepDefinitions.cs
@@ -5,6 +5,7 @@ using System.IO;
 using MBlogBuilder;
 using MBlogModel;
 using MBlogRepository.Repositories;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Remote;
 using TechTalk.SpecFlow;
 
@@ -13,61 +14,34 @@ namespace MBlogSpecs
     [Binding]
     public class MBlogMediaStepDefinitions
     {
-        private const string Image = "../../Images/image.png";
-        private byte[] _imageData;
+        //        [BeforeScenario("image")]
 
-        [BeforeScenario("image")]
-        public void BeforeFeatureImage()
+        [Given(@"That I navigate to a blog post that contains an image")]
+        public void GivenThatINavigateToABlogPostThatContainsAnImage()
         {
-            //using(FileStream str = File.Open(Image, FileMode.Open))
-            //{
-            //    _imageData = new byte[str.Length];
-            //    str.Read(_imageData, 0, _imageData.Length);
-            //}
-            //User user = BuildMeA.User("email", "name", "password");
+            var driver = new RemoteWebDriver(new Uri("http://localhost"), DesiredCapabilities.HtmlUnitWithJavaScript());
 
-            //UserRepository userRepository = new UserRepository(ConfigurationManager.ConnectionStrings["testdb"].ConnectionString);
+            // Find the text input element by its name
+            driver.Navigate();
 
-            //userRepository.Create(user);
+            // Enter something to search for
+            IWebElement element = driver.FindElement(By.Id(""));
 
-            //using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testdb"].ConnectionString))
-            //{
-            //    using (SqlCommand cmd = connection.CreateCommand())
-            //    {
-            //        connection.Open();
-            //        cmd.CommandText =
-            //            "INSERT INTO [Media]([title],[file_name], [link_key], [year], [month], [day]," +
-            //            "[mime_type],[alignment],[size],[user_id],[bytes])" +
-            //             "VALUES(@title, @file_name,  @link_key, @year,  @month,  @day, @mime_type, @alignment, @size, @user_id, @bytes)";
-            //        cmd.Parameters.AddWithValue("@title", "TestImage");
-            //        cmd.Parameters.AddWithValue("@file_name", "file_name");
-            //        cmd.Parameters.AddWithValue("@link_key", "link");
-            //        cmd.Parameters.AddWithValue("@year", 2012);
-            //        cmd.Parameters.AddWithValue("@month", 12);
-            //        cmd.Parameters.AddWithValue("@day", 18);
-            //        cmd.Parameters.AddWithValue("@mime_type", "mime");
-            //        cmd.Parameters.AddWithValue("@alignment", 1);
-            //        cmd.Parameters.AddWithValue("@size", 1);
-            //        cmd.Parameters.AddWithValue("@user_id", user.Id);
-            //        cmd.Parameters.AddWithValue("@bytes", _imageData);
-
-            //        cmd.ExecuteNonQuery();
-            //    }
-            //}
+            // Now submit the form. WebDriver will find the form for us from the element
+            //element.S
         }
 
-        [AfterScenario("image")]
-        public void AfterFeatureImage()
+        [When(@"I view the blog post")]
+        public void WhenIViewTheBlogPost()
         {
-            //using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["testdb"].ConnectionString))
-            //{
-            //    using (SqlCommand cmd = connection.CreateCommand())
-            //    {
-            //        connection.Open();
-            //        cmd.CommandText = "delete media; delete users; ";
-            //        cmd.ExecuteNonQuery();
-            //    }
-            //}
+            ScenarioContext.Current.Pending();
         }
+
+        [Then(@"the image should be visible")]
+        public void ThenTheImageShouldBeVisible()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
     }
 }
