3bd9b017dd7a3b9d144a33ab4e2aff05dc61ca8b
Kevin Jones <kevinj@mantiso.com>
2012-05-15 14:29:19 +0100

    Added HTML helper to handle login UI bits

---
+++ MBlog/HtmHelpers/DisplayAuthentication.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace MBlog.HtmHelpers
+{
+    public static class DisplayAuthentication
+    {
+        public static MvcHtmlString AuthenticationStatus(this HtmlHelper html)
+        {            
+            string outputString;
+            if (HttpContext.Current.User == null || !HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                outputString = string.Format("<li>{0}</li> <li>{1}</li>", html.ActionLink("register", "New", "user", null, null), html.ActionLink("login", "New", "Session", null, null));
+            }
+            else
+            {
+                outputString = string.Format("<li>{0}</li> <li>{1}</li>", html.ActionLink("dashboard", "Index", "Dashboard", null, null), html.ActionLink("logout", "Delete", "session", null, null));
+            }
+            return new MvcHtmlString(outputString);
+        }
+    }
+}
